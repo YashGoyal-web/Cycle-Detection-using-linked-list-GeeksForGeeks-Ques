7cb5e9f4493d8adf03c039b698c32f9d2bc35222
YashGoyal-web <62822789+YashGoyal-web@users.noreply.github.com>
2020-08-23 00:10:57 +0530

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,94 @@
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the hasCycle function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean hasCycle(SinglyLinkedListNode head) {
+
+                 ArrayList<SinglyLinkedListNode> al = new ArrayList();
+
+        if(head == null){
+            return false;
+        }else{
+            SinglyLinkedListNode temp = head;
+            
+            while(temp != null){
+              
+              al.add(temp);
+
+               for(int i=0 ; i<al.size()-1 ; i++){
+                   if(temp == al.get(i)){
+                       return true;
+                   }
+               }
+              temp = temp.next;            
+            }
+
+           return false;
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
